9712d33c684c03a407d97a1ff180364508ea4a55
Hazi-rt003 <twnyoike@gmail.com>
2026-07-15 11:36:49 +0300

    authentication changes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from sqlalchemy import text
-
+from app.api.users import router as users_router
 from app.database.database import engine
 
 app = FastAPI(
     title="SecureBank API",
     version="1.0.0",
 )
+
+app.include_router(users_router)
 
 
 @app.get("/")
